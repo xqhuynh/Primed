0d77795df4550b76a24d1fca3b1779e9ac16e276
Savon Huynh <xqhuynh@gmail.com>
2022-11-28 21:05:41 -0800

    Add user table to database.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,17 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- USER TABLE
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY, 
+	"username" VARCHAR NOT NULL UNIQUE,
+	"password" VARCHAR NOT NULL,
+	"avatar" VARCHAR, 
+	"firstName" VARCHAR, 
+	"lastName" VARCHAR,
+	"pronouns" VARCHAR,
+	"adminLevel" VARCHAR,
+	"linkedIn" VARCHAR,
+	"website" VARCHAR,
 );
