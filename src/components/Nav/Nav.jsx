010619d312ef42bc3ed9466cf1cdee5de4c68d45
Savon Huynh <xqhuynh@gmail.com>
2022-12-04 21:24:11 -0800

    Change nav links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,22 +14,20 @@ function Nav() {
       </Link>
       <div>
 
-
-        
         {/* If no user is logged in, show these links */}
         {!user.id && (
           // If there's no user, show login/registration links
           <>
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-
           <Link className="navLink" to="/about">
           About
           </Link>
 
           <Link className="navLink" to="/info">
               Info Page
+          </Link>
+
+          <Link className="navLink" to="/login">
+            Login / Register
           </Link>
           </>
 
